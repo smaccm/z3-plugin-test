3a3c85d8a977ef241bee3e0290096774a74429b0
Karl Hoech <karl.hoech@collins.com>
2019-02-19 06:35:35 -0600

    Fix plugin directories and rebuild

--- com.collins.trustedsystems.z3/src/com/collins/trustedsystems/z3/Z3Plugin.java
+++ com.collins.trustedsystems.z3/src/com/collins/trustedsystems/z3/Z3Plugin.java
@@ -10,12 +10,12 @@ import org.osgi.framework.Bundle;
 public class Z3Plugin {
 
 	public static String getZ3Directory() {
-		Bundle bundle = Platform.getBundle("com.collins.trustedsystems.z3");
-		String archDir = getArchDir();
+		String fragmentExt = getFragmentExt();
+		Bundle bundle = Platform.getBundle("com.collins.trustedsystems.z3" + "." + fragmentExt);
 		String exeName = getExecutableName();
 		try {
 			// Extract entire directory so DLLs are available on windows
-			URL dirUrl = FileLocator.toFileURL(bundle.getEntry(archDir));
+			URL dirUrl = FileLocator.toFileURL(bundle.getEntry("binaries"));
 			File exe = new File(dirUrl.getPath(), exeName);
 			exe.setExecutable(true);
 			return exe.getParent();
@@ -24,22 +24,22 @@ public class Z3Plugin {
 		}
 	}
 
-	private static String getArchDir() {
+	private static String getFragmentExt() {
 		String name = System.getProperty("os.name").toLowerCase();
 		String arch = System.getProperty("os.arch").toLowerCase();
 
 		if (name.contains("win32") || name.contains("windows")) {
 			if (arch.contains("64")) {
-				return "win32.x86_64";
+				return "win32.win32.x86_64";
 			} else {
-				return "win32.x86";
+				return "win32.win32.x86";
 			}
 		} else if (name.contains("mac os x")) {
-			return "macosx.x86_64";
+			return "macosx.cocoa.x86_64";
 		} else if (arch.contains("64")) {
-			return "linux.x86_64";
+			return "linux.gtk.x86_64";
 		} else {
-			return "linux.x86";
+			return "linux.gtk.x86";
 		}
 	}
 
